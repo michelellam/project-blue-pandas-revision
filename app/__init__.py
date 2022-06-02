d547eb355cf9c7820dfc5bed99d2c92447671c9c
Michelle Lam <michellelamhelps@gmail.com>
2022-06-02 16:29:45 -0400

    education done

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
-import os
+#import os
 from flask import Flask, render_template, request
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-load_dotenv()
+#load_dotenv()
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+#@app.route('/')
+#def index():
+    #return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+
+
