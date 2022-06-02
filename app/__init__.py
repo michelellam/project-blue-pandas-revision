9e0bac423040cd73ac295991bcda5947b23681a4
Michelle Lam <michellelamhelps@gmail.com>
2022-06-02 19:44:50 -0400

    education + hobbies done

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,28 @@
-#import os
+import os
 from flask import Flask, render_template, request
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-#load_dotenv()
-app = Flask(__name__)
+load_dotenv()
+app = Flask(__name__, template_folder='templates')
 
 
-#@app.route('/')
-#def index():
-    #return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+@app.route('/')
+def index():
+    return render_template('index.jinja', title="MLH Fellow", url=os.getenv("URL"))
 
+#@app.route ("/")
+@app.route("/education")
+def education():
+     return render_template("education.jinja")
+
+@app.route("/hobbies")
+def hobbies():
+     return render_template("hobbies.jinja")
+
+@app.route("/workexperience")
+def workexperience():
+     return render_template("work_experience.jinja")
+
+if __name__ == "__main__":
+     app.run(debug = True)
 
