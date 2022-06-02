c05b2a21b98e25a0159002c7c5cbecb9488cd9c0
Ruy Guzmán Camacho <ruy.guzman2002@gmail.com>
2022-06-01 22:55:54 -0500

    landing page first draft

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,12 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,json
 from dotenv import load_dotenv
 
 load_dotenv()
 app = Flask(__name__)
 
+data = json.load(open('./app/static/data.json'))
 
 @app.route('/')
 def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+    return render_template('index.html', title="MEET THE TEAM!",user1 = data["name1"],user2 = data["name2"], url=os.getenv("URL"))
