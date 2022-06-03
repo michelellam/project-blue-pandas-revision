aa4e70d02d8baa9e616cd864bbcf543dc73ffdbb
Michelle Lam <michellelamhelps@gmail.com>
2022-06-03 12:30:42 -0400

    workexperience done (just need to style it)

--- app/__init__.py
+++ app/__init__.py
@@ -19,11 +19,16 @@ def education():
 
 @app.route("/hobbies")
 def hobbies():
-     return render_template("hobbies.jinja", )
+     return render_template("hobbies.jinja" )
 
 @app.route("/workexperience")
 def workexperience():
-     return render_template("work_experience.jinja")
+     return render_template("work_experience.jinja", work=[{"name2": "Michelle", "company": "CS 110 Final Project", "description": "Using  the pygame library to build a basic platform game",
+     "company2": "MLH Fellowship", "description2": "Part of the Production Engineer Track under the mentorship of Meta Engineers", "company3": "Tutor", "description3":" Voluteer to tutor elementary students on math"},
+
+     {"name2": "Ruy", "company": "El club de los abuelos", "description": "develop a full-stack web application oriented to keep track of their patients health and examination procedures", 
+     "company2": "Banco de Alimentos Guadalajara", "description2": "worked on the design of wireframes and system requirements of the web site of their branch Banco de Empleos Guadalajara", 
+     "company3": "JANN (Jovenes ayudando a niñas y niños)", "description3": " free Math tutoring to kids from elementary and middle school" }])
 
 if __name__ == "__main__":
      app.run(debug = True)
