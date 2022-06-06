8223b9f67a0830284e451a83edf5c531139c9790
Michelle Lam <michellelamhelps@gmail.com>
2022-06-05 20:01:45 -0400

    hobbies done !

--- app/__init__.py
+++ app/__init__.py
@@ -17,20 +17,16 @@ def homepage(user):
     return render_template('home.jinja',title = data[user]["name"],user = data[user],url=os.getenv("URL"))
 
 #@app.route ("/")
-@app.route("/<user>/education")
+@app.route("/<user>/educationexperience")
 def education(user):
 
-     return render_template("education.jinja",  title = data[user]["name"], majors = data[user]["major"], uni_name = data[user]["school"])
+     return render_template("education_experience.jinja",  title = data[user]["name"], majors = data[user]["major"], uni_name = data[user]["school"],companies = data[user]["companies"], workexperiences = data[user]["work_experiences"] )
 
 @app.route("/<user>/hobbies")
 def hobbies(user):
 
-     return render_template("hobbies.jinja", title = data[user]["name"], pics = data[user]["hobbies_pics"])
+     return render_template("hobbies.jinja", title = data[user]["name"], pics = data[user]["hobbies_pics"], hobbies_name = data[user]["hobbies_description"], hobbies_memos = data[user]["hobbies_notes"])
 
-
-@app.route("/<user>/workexperience")
-def workexperience(user):
-      return render_template("work_experience.jinja", title = data[user]["name"], companies = data[user]["companies"], workexperiences = data[user]["work_experiences"] )
 
 if __name__ == "__main__":
      app.run(debug = True)
