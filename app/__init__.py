3ac9f08115b4364f336822275fd4c73496d78689
Michelle Lam <michellelamhelps@gmail.com>
2022-07-06 16:45:57 -0400

    fixed INIT !!!!!

--- app/__init__.py
+++ app/__init__.py
@@ -10,16 +10,16 @@ load_dotenv()
 app = Flask(__name__, template_folder='template')
 
 if os.getenv("TESTING") == "true":
-     print("Running in test mode")
-     mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
+      print("Running in test mode")
+      mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
 else:
-     mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
-     user=os.getenv("MYSQL_DATABASE"),
-     password=os.getenv("MYSQL_PASSWORD"),
-     host=os.getenv("MYSQL_HOST"),
-     port=3306)
+      mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+      user=os.getenv("MYSQL_USER"),
+      password=os.getenv("MYSQL_PASSWORD"),
+      host=os.getenv("MYSQL_HOST"),
+      port=3306)
 
-     print(mydb)
+      print(mydb)
 
 data = json.load(open('./app/static/data.json'))
 
