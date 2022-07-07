7f0c9857ee0c5fab23a342a6759484f66a3599b5
Michelle Lam <michellelamhelps@gmail.com>
2022-07-07 12:23:57 -0400

    fixed typos in deploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -9,4 +9,4 @@ git fetch && git reset origin/main --hard
 
 docker compose -f docker-compose.prod.yml down
 
-docker compose -f docker-compose.prod.yml -d --build
+docker compose -f docker-compose.prod.yml up -d --build
