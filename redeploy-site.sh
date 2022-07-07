b700b69a22ae0ea9e652190174a2447a6a249eaf
Michelle Lam <michellelamhelps@gmail.com>
2022-07-07 11:54:00 -0400

    updated redeploy script

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -7,13 +7,6 @@ cd project-blue-pandas-revision-2
 #making sure we have the updated changes from the main branch 
 git fetch && git reset origin/main --hard
 
-#entering the python virtual environment
-source python3-virtualenv/bin/activate
+docker compose -f docker-compose.prod.yml down
 
-#install dependencies
-python -m pip install --upgrade pip
-pip install -r requirements.txt 
-
-##restart the service + checking up the status
-systemctl restart myportfolio
-systemctl status myportfolio
+docker compose -f docker-compose.prod.yml -d --build
